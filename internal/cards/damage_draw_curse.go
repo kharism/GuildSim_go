2ee5d80dadb21bd4c281c753dbeb6b70641494ae
kharisma <greyroot00@gmail.com>
2023-03-11 20:10:42 +0700

    remove uneeded implementation of function

--- internal/cards/damage_draw_curse.go
+++ internal/cards/damage_draw_curse.go
@@ -14,18 +14,6 @@ func (d *DamageDrawCurse) GetName() string {
 func (d *DamageDrawCurse) GetDescription() string {
 	return "If drawn: Inflict 2 damage then banish this card"
 }
-func (d *DamageDrawCurse) GetCost() Cost {
-	cost := NewCost()
-	return cost
-}
-func (d *DamageDrawCurse) OnPunish() {
-
-}
-
-// when discarded to cooldown pile, do this
-func (d *DamageDrawCurse) OnDiscarded() {
-
-}
 
 // when added to hand do this
 func (d *DamageDrawCurse) OnAddedToHand() {
