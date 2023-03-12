d4c13241f563ffcccf2b9815f606806e0d1fe060
kharisma <greyroot00@gmail.com>
2023-03-12 18:52:29 +0700

    Add rookie mage

--- internal/cards/cards_test/cards_test.go
+++ internal/cards/cards_test/cards_test.go
@@ -255,3 +255,42 @@ func TestEndTurn(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestRookieMage(t *testing.T) {
+	gamestate := NewDummyGamestate()
+	cardPicker := TestCardPicker{}
+
+	cardPicker.ChooseMethod = StaticCardPicker(0)
+
+	dumGamestate := gamestate.(*DummyGamestate)
+
+	dumGamestate.CardsInDeck = cards.Deck{}
+	dumGamestate.cardPiker = &cardPicker
+
+	k := cards.NewRookieMage(gamestate)
+	dumGamestate.CardsInDeck.Push(&k)
+
+	for i := 0; i < 10; i++ {
+		j := cards.NewRookieAdventurer(gamestate)
+		dumGamestate.CardsInDeck.Push(&j)
+	}
+
+	for i := 0; i < 5; i++ {
+		gamestate.Draw()
+	}
+	cardsInHand := gamestate.GetCardInHand()
+	if len(cardsInHand) != 5 {
+		t.Log("Failed to draw")
+		t.FailNow()
+	}
+	gamestate.PlayCard(cardsInHand[0])
+	if len(cardsInHand) != 5 {
+		t.Log("Failed to draw")
+		t.FailNow()
+	}
+
+	if dumGamestate.CardsDiscarded.Size() != 1 {
+		t.Log("Failed to discard")
+		t.FailNow()
+	}
+}
---
+++ internal/cards/rookie_mage.go
@@ -0,0 +1,43 @@
+package cards
+
+type RookieMage struct {
+	BaseHero
+	state AbstractGamestate
+}
+
+func NewRookieMage(state AbstractGamestate) RookieMage {
+	j := RookieMage{state: state}
+	return j
+}
+
+func (h *RookieMage) GetName() string {
+	return "Rookie Mage"
+}
+func (h *RookieMage) GetDescription() string {
+	return "Discard 1 then draw 2"
+}
+func (h *RookieMage) GetCost() Cost {
+	cost := NewCost()
+	cost.AddResource(RESOURCE_NAME_EXPLORATION, 2)
+	return cost
+}
+
+func (h *RookieMage) OnPlay() {
+	cardPicker := h.state.GetCardPicker()
+	cardInHand := h.state.GetCardInHand()
+
+	discardedIdx := cardPicker.PickCard(cardInHand, "Choose Card to discard")
+
+	discardedCard := cardInHand[discardedIdx]
+
+	h.state.RemoveCardFromHandIdx(discardedIdx)
+
+	h.state.DiscardCard(discardedCard)
+	for i := 0; i < 2; i++ {
+		h.state.Draw()
+	}
+}
+
+func (h *RookieMage) Dispose() {
+	h.state.DiscardCard(h)
+}
