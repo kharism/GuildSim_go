aa36f1a5a3944ed4c74b1a1704b25ce09c311835
kharisma <greyroot00@gmail.com>
2023-03-14 19:37:30 +0700

    Add important things for tomb of forgotten monarch
    - add card defeated listener
    - add countdown action
    - add stun curse
    - add tomb of forgotten monarch_entrance
    - add skeleton guard
    - add push center deck action

---
+++ internal/cards/lich_mage_monster.go
@@ -0,0 +1,32 @@
+package cards
+
+type LichMageMonster struct {
+	BaseMonster
+	state       AbstractGamestate
+	turnCounter int
+}
+
+func NewLichMageMonster(state AbstractGamestate) LichMageMonster {
+	return LichMageMonster{state: state}
+}
+
+func (m *LichMageMonster) GetName() string {
+	return "Lich Mage"
+}
+func (m *LichMageMonster) GetDescription() string {
+	return "Add 1 Stun each turn, and additional 1 stun every 3 turns, on slain unlocks Tomb of forgotten monarch: central chamber"
+}
+func (m *LichMageMonster) GetCost() Cost {
+	cost := NewCost()
+	cost.AddResource(RESOURCE_NAME_COMBAT, 5)
+	return cost
+}
+func (m *LichMageMonster) OnPunish() {
+	m.turnCounter += 1
+	curse1 := NewStunCurse(m.state)
+	m.state.DiscardCard(&curse1)
+	if m.turnCounter%3 == 0 {
+		curse2 := NewStunCurse(m.state)
+		m.state.DiscardCard(&curse2)
+	}
+}
---
+++ internal/cards/skeleton_guard_monster.go
@@ -0,0 +1,26 @@
+package cards
+
+type SkeletonGuard struct {
+	BaseMonster
+	state AbstractGamestate
+}
+
+func NewSkeletonGuard(state AbstractGamestate) SkeletonGuard {
+	k := SkeletonGuard{state: state}
+	return k
+}
+func (m *SkeletonGuard) GetName() string {
+	return "Skeleton Guard"
+}
+func (m *SkeletonGuard) GetDescription() string {
+	return "Deals 3 dmg, on slain gains 1 exploration and 1 reputation"
+}
+func (m *SkeletonGuard) GetCost() Cost {
+	cost := NewCost()
+	cost.AddResource(RESOURCE_NAME_COMBAT, 3)
+	return cost
+}
+func (m *SkeletonGuard) OnSlain() {
+	m.state.AddResource(RESOURCE_NAME_EXPLORATION, 1)
+	m.state.AddResource(RESOURCE_NAME_REPUTATION, 1)
+}
