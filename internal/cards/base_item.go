c633408aba7f7acbd94cdf6631a0e08396051f36
kharisma <greyroot00@gmail.com>
2023-03-29 05:38:40 +0700

    add several items to the list
    - add onAcquire method on baseItem
    
    - add combat/banish/explore potion
    
    - add AddItem on gamestate
    
    - add relic combattalisman and explorerboot
    
    - add basiclistener class

--- internal/cards/base_item.go
+++ internal/cards/base_item.go
@@ -22,11 +22,8 @@ func (h *BaseItem) OnAddedToHand() {}
 // when played from hand, do this
 func (h *BaseItem) OnPlay() {}
 
-<<<<<<< HEAD
 func (h *BaseItem) OnAcquire() {}
 
-=======
->>>>>>> 3ea1550 (add item related stuff.)
 // when explored, do this
 func (h *BaseItem) OnExplored() {}
 
--- internal/cards/item/healing_potion.go
+++ internal/cards/item/healing_potion.go
@@ -9,17 +9,12 @@ type HealingPotion struct {
 	state cards.AbstractGamestate
 }
 
-<<<<<<< HEAD
 func NewHealingPotion(state cards.AbstractGamestate) HealingPotion {
 	return HealingPotion{state: state}
 }
 
 func (h *HealingPotion) Dispose(source string) {
 	h.state.BanishCard(h, source)
-=======
-func (h *HealingPotion) Dispose(source string) {
-
->>>>>>> 3ea1550 (add item related stuff.)
 }
 func (h *HealingPotion) GetName() string {
 	return "Healing Potion"
@@ -32,13 +27,8 @@ func (h *HealingPotion) GetCost() cards.Cost {
 	return cost
 }
 
-<<<<<<< HEAD
 func (h *HealingPotion) OnConsume() {
 	h.state.TakeDamage(-5)
 	h.state.RemoveItem(h)
 	h.Dispose(cards.DISCARD_SOURCE_NAN)
-=======
-func (h *HealingPotion) OnConsume(source string) {
-	h.state.TakeDamage(-5)
->>>>>>> 3ea1550 (add item related stuff.)
 }
